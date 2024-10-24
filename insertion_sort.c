5e95e5b339106304abbaa03677c0b248a28cc05c
AnarchistAristocrat <sebkittel123@gmail.com>
2024-10-24 15:28:16 +0200

    Lavet opgave 2

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,6 +1,39 @@
 #include "insertion_sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 node* isort(node *list) {
+    if(list == NULL)
+        return NULL;
+
+    node *next_element = list->next;
+    list->next = NULL;
+
+    while(next_element != NULL){
+        node *current = next_element;
+        next_element = next_element->next;
+        if(current->data < list->data){
+            current->next = list;
+            list = current;
+            continue;
+        }
+        node *list_element = list;
+        while(list_element->next != NULL){
+            if(current->data < list_element->next->data){
+                current->next = list_element->next;
+                list_element->next = current;
+                break;
+            }
+            list_element = list_element->next;
+        }
+        if(list_element->next == NULL){
+            list_element->next = current;
+            current->next = NULL;
+        }
+
+    }
+    
     return list;
 }
+
 
