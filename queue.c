42b8a31ce38e0da47c4f9a251f81b02e729446d9
AnarchistAristocrat <sebkittel123@gmail.com>
2024-10-25 15:01:20 +0200

    Lavede opgave 4 ordentligt

--- queue.c
+++ queue.c
@@ -79,93 +79,74 @@ int dequeue(queue *q) {
 }
 
 // Opgave 4
-
-/*Note til instruktore: Havde sgu lidt svært ved både at forstå 
-opgaven, samt hvordan de tomme funktioner I havde skrevet skulle 
-bruges til at løse den. Men endte med at forstå formuleringen i 
-PDF'en sådan, at det måtte være noget med at bruge stack-funktioner
-til at opnå 'queue-behavior'. Så vi endte med at skrive enqueueStack
-og dequeueStack således at nå de kaldtes på en stack, så ville de give
-'queue-behavior': enqueue tilføjer elementer til stack, og dequeue fjerner
-det først tilføjede element fra stack. Vi har dog ændret på de tomme funktioner
-I skrev op, så det var nok ikke det I tænkte, men ved ikke lige ellers hvordan
-vi skulle bruge stacks, samt de funktioner vi skrev i opgave 7 (som der stod vi
-kunne gøre i opgaveformuleringen).*/
-
-// Funktion fra sidste uge
-stack *initializeStack() {
-  stack *s = malloc(sizeof(stack));
-  // Sætter stack til at være tom
-  s->head = NULL;
-  return s;
+void push(int element, node **head) {
+    // Laver ny node der har element som data
+    node *n = malloc(sizeof(node));
+    n->data = element;
+    // Sætter n til at pege på head og gør n til ny head
+    n->next = *head;
+    *head = n;
 }
 
-// Funktion fra sidste uge
-void push(int x, stack *s) {
-  // Laver ny node der har x som data
-  node *n = malloc(sizeof(node));
-  n->data = x;
-  // Nye element peger på gamle head
-  n->next = s->head;
-  // Nye node bliver sat som head
-  s->head = n;
-}
-
-// Funktion fra sidste uge
-int pop(stack *s) {  
-  // Flytter node som skal poppes over i ny node
-  node* n = s->head;
-  int return_val = n->data;
-  // head for stacken bliver sat til forrige element
-  s->head = n->next;
-  free(n);
-  // Returnerer dataen fra den poppede node
-  return return_val;
-}
-
-// Funktion fra sidste uge
-bool emptyStack(stack *s) {
-  // Returnerer sand hvis head er NULL
-  return s->head == NULL;
-}
-
-
-
-void enqueueStack(stack *s, int x) {
-    /*Tilføjer element til stack*/
-    push(x, s);
-}
-
-int dequeueStack(stack *s) {
-    /*Tjekker at stack ikke er tom*/
-    assert(!emptyStack(s));
-    int return_val;
-    /*Laver midlertidig stack, hvor alle elementer fra stack
-    flyttes over for at ændre deres orden*/
-    stack *temp_stack = initializeStack();
-    while(!(emptyStack(s)))
-        push(pop(s), temp_stack);
-    /*Popper øverste element fra temp_stack, som må være det
-    element som først blev tilføjet til den oprindelige stack*/
-    return_val = pop(temp_stack);
-    /*Flytter elementer tilbage til den oprindelige stack*/
-    while(!emptyStack(temp_stack))
-        push(pop(temp_stack), s);
-    free(temp_stack);
+int pop(node **head) {
+    // Pointer til node som skal poppes
+    node *n = *head;
+    // Returnværdi sættes
+    int return_val = n->data;
+    // Nyt head delkareres
+    *head = n->next;
+    // Gamle head slettes
+    free(n);
     return return_val;
 }
 
-// Hvis nedenstående main køres tester den, at koden fra opgave 4 virker
+bool emptyStack(node **head){
+    return *head == NULL;
+}
 
-/*
+void initializeStack(stack *s) {
+  // Sætter stack til at være tom
+  s->head = NULL;
+}
+
+void enqueueStack(queue *q, int x) {
+    push(x, &q->rear);
+    if(empty(q)){
+        push(x, &q->front);
+    }
+    q->size++;
+}
+
+int dequeueStack(queue *q) {
+    assert(!empty(q));
+    stack temp_stack;
+    initializeStack(&temp_stack);
+    while(!emptyStack(&q->rear))
+        push(pop(&q->rear), &temp_stack.head);
+    int return_val = pop(&temp_stack.head);
+
+    if(!emptyStack(&temp_stack.head)){
+        push(pop(&temp_stack.head), &q->rear);
+        q->front = q->rear;
+        while(!emptyStack(&temp_stack.head)){
+            push(pop(&temp_stack.head), &q->rear);
+        }
+    }
+    
+    q->size--;
+    return return_val;
+}
+
 int main(){
-    stack *s = initializeStack();
-    enqueueStack(s, 1);
-    enqueueStack(s, 2);
-    enqueueStack(s, 3);
-    printf("%d", dequeueStack(s));
-    printf("%d", dequeueStack(s));
-    printf("%d", dequeueStack(s));
+    queue *q;
+    initialize(q);
+    enqueue(q, 1);
+    enqueueStack(q, 2);
+    enqueue(q,3);
+    enqueueStack(q, 4);
+    printf("%d", dequeueStack(q));
+    printf("%d", dequeue(q));
+    printf("%d", dequeueStack(q));
+    printf("%d", dequeue(q));
     return 0;
 }
-*/
